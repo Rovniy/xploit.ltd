ed181a0543a5a4f5962078f37405fe1625872f2b
Andrew Rovniy <smart.fz@mail.ru>
2023-11-22 01:13:39 +0100

    Add xploit logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'XPLOIT Ltd.'
+    }
+  },
   devtools: { enabled: true }
 })
